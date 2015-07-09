0ef8232b90cad146d30d6cce241e2c618dc86e38
Andrey Rodin <rodin@aventoo.com>
2015-07-10 00:42:15 +0100

    Plot 1 updated to use transparent background color.

--- plot1.R
+++ plot1.R
@@ -4,6 +4,9 @@ data <- loadData()
 
 # Plot 1
 png(filename = "plot1.png", width = 480, height = 480)
+# Set up plot parameters
+par(bg = "transparent")
+# Draw a histogram
 hist(data$Global_active_power,
      col = "red",
      main = "Global Active Power",
